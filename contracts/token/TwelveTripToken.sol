8b0441c7f074fb795942c519a588ad9b6e31db07
Nguyen Nghia <nghiand2@topica.edu.vn>
2018-05-16 21:13:33 +0700

    change name of token

--- contracts/token/TwelveTripToken.sol
+++ contracts/token/TwelveTripToken.sol
@@ -7,7 +7,7 @@ contract TwelveTripToken is MintableToken {
     //                                         Members
     // =================================================================================================================
 
-    string public name = "Twelve Trip";
+    string public name = "12 Trip";
 
     string public symbol = "TRIP";
 
